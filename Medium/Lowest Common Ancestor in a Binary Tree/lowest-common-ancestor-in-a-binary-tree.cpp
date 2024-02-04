e434ae08987abbd558a26213140811f684c32049
Rohith Kumar <pathinarohith@gmail.com>
2024-02-04 11:07:44 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Lowest Common Ancestor in a Binary Tree/lowest-common-ancestor-in-a-binary-tree.cpp
@@ -0,0 +1,186 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// Tree Node
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+    Node(){
+        data = 0;
+        left = right = NULL;
+    }
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+
+// } Driver Code Ends
+/* A binary tree node
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+ */
+
+class Solution
+{
+    Node* sol=NULL;
+    
+    pair<bool,bool> findNodes(Node* root,int n1, int n2){
+        if(root==NULL){
+            return make_pair(false,false);
+        }
+        pair<bool,bool> foundNodes;
+        if(n1==root->data){
+            foundNodes.first=true;
+        }
+        if(n2==root->data){
+            foundNodes.second=true;
+        }
+        pair<bool,bool> left=findNodes(root->left,n1,n2);
+        pair<bool,bool> right=findNodes(root->right,n1,n2);
+        if(left.first==true||right.first==true){
+            foundNodes.first=true;
+        }
+        if(right.second==true||left.second==true){
+            foundNodes.second=true;
+        }
+        
+        if(foundNodes.first&&foundNodes.second&&!sol){
+            sol=root;
+        }
+        return foundNodes;
+        
+    }
+    public:
+    
+    Node* lca(Node* root ,int n1 ,int n2 )
+    {
+        pair<bool,bool> foundNodes;
+        
+        foundNodes=findNodes(root,n1,n2);
+        
+        return sol;
+    }
+};
+
+//{ Driver Code Starts.
+
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+
+// Function to Build Tree
+Node* buildTree(string str)
+{
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+
+    // for(string i:ip)
+    //     cout<<i<<" ";
+    // cout<<endl;
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if(currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if(currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+// Function for Inorder Traversal
+void printInorder(Node* root)
+{
+    if(!root)
+        return;
+
+    printInorder(root->left);
+    cout<<root->data<<" ";
+    printInorder(root->right);
+}
+
+int main() {
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int a,b;
+        scanf("%d %d ",&a,&b);
+        string s;
+        getline(cin,s);
+        Node* root = buildTree(s);
+        Solution ob;
+        cout<<ob.lca(root,a,b)->data<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends

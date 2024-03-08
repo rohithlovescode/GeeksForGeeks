75f8f2cdbefedf3217de69f2ed5f8729338e6f15
Rohith Kumar <pathinarohith@gmail.com>
2024-03-08 13:44:40 +0530

    Updated Solution - GfG to GitHub

--- Medium/Kadane's Algorithm/kadanes-algorithm.cpp
+++ Medium/Kadane's Algorithm/kadanes-algorithm.cpp
@@ -9,42 +9,22 @@ class Solution{
     // arr: input array
     // n: size of array
     //Function to find the sum of contiguous subarray with maximum sum.
-    int kadaneAlgo(int arr[],int left,int right){
-        if(left>=right){
-            return arr[left];
-        }
-        int mid=(left+right)/2;
-        
-        long long LSS=kadaneAlgo(arr,left,mid);
-        long long RSS=kadaneAlgo(arr,mid+1,right);
-        
-        long long leftSum=0,leftMax=arr[mid];
-        long long rightSum=0,rightMax=arr[mid+1];
-        
-        for(int i=mid;i>=left;i--){
-            leftSum=leftSum+arr[i];
-            if(leftSum>leftMax){
-                leftMax=leftSum;
-            }
-        }
-        for(int i=mid+1;i<=right;i++){
-            rightSum=rightSum+arr[i];
-            if(rightSum>rightMax){
-                rightMax=rightSum;
-            }
-        }
-        
-        long long MSS=leftMax+rightMax;
-        long long sol=max(max(LSS,RSS),MSS);
-        return sol;
-    }
     long long maxSubarraySum(int arr[], int n){
-        if(n==500000){
-            return 2492958345;
+        if(n==0){
+            return 0;
         }
-        long long ans=kadaneAlgo(arr,0,n-1);
-        
-        return ans;
+        long long maxSum=arr[0];
+        long long currSum=0;
+        for(int i=0;i<n;i++){
+            currSum+=arr[i];
+            if(currSum>maxSum){
+                maxSum=currSum;
+            }
+            if(currSum<0){
+                currSum=0;
+            }
+        }
+        return maxSum;
     }
 };
 
